e5627b7f9973263eedac8103d709f7b11ed9d685
tatyanepgoncalves <tatygoncalves715@gmail.com>
2025-12-27 11:25:14 -0300

    feat: add middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,35 @@
-import { Router, type Request, type Response } from "express";
+import { Router, type NextFunction, type Request, type Response } from "express";
 
 export const router: Router = Router()
 
 const tarefas = ["Estudar Node JS","Fazer compras","Ler um livro"];
+
+// Middleware
+// Está no meio após chamar a rota e antes  de chamar o callback
+
+router.use((req: Request, res: Response, next: NextFunction) => {
+  console.log("Passou no Middleware de Rotas")
+  return next()
+})
+
+// Middleware 
+function checkTask(req: Request, res: Response, next: NextFunction) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'Nome inválido / faltando nome' })
+  }
+
+  return next()
+}
+
+function checkIndexTask(req: Request, res: Response, next: NextFunction) {
+  const tarefa = tarefas[Number(req.params.index)]
+
+  if (!tarefa) {
+    return res.status(400).json({ error: 'Tarefa não encontrada!' })
+  }
+
+  return next()
+}
 
 // Request Body => /tarefas
 // Lista todas as tarefas
@@ -11,21 +38,15 @@ router.get("/tarefas", (req: Request, res: Response) => {
 })
 
 // Lista uma tarefa específica
-router.get("/tarefas/:index", (req: Request, res: Response) => {
+router.get("/tarefas/:index", checkIndexTask, (req: Request, res: Response) => {
   const index = req.params.index
 
   res.json(tarefas[Number(index)])
 })
  
 // Adiciona uma nova tarefa
-router.post("/tarefas", (req: Request, res: Response) => {
+router.post("/tarefas", checkTask, checkIndexTask, (req: Request, res: Response) => {
   const { name } = req.body
-
-  if (!name) {
-    res.status(400).json({ message: 'O nome da tarefa não pode ser vazio.' })
-
-    return;
-  }
 
   tarefas.push(name)
 
@@ -33,22 +54,16 @@ router.post("/tarefas", (req: Request, res: Response) => {
 })
 
 // Atualiza uma tarefa existente
-router.put("/tarefas/:index", (req: Request, res: Response) => {
+router.put("/tarefas/:index", checkTask, checkIndexTask, (req: Request, res: Response) => {
   const index = req.params.index
   const { name } = req.body
-
-  if (!name) {
-    res.status(400).json({ message: 'O nome da tarefa não pode ser vazio.' })
-
-    return;
-  }
 
   tarefas[Number(index)] = name
   res.json({ message: "Tarefa atualizada com sucesso!", tarefa: name })
 })
 
 // Remove uma tarefa
-router.delete("/tarefas/:index", (req: Request, res: Response) => {
+router.delete("/tarefas/:index", checkIndexTask, (req: Request, res: Response) => {
   const index = req.params.index
 
   tarefas.splice(Number(index), 1)
