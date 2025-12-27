4a79105f59270d2e6670c133a3388023c0788999
tatyanepgoncalves <tatygoncalves715@gmail.com>
2025-12-27 11:03:59 -0300

    feat: PUT method

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Router, type Request, type Response } from "express";
 
+export const router: Router = Router()
 
-const router: Router = Router()
 const tarefas = ["Estudar Node JS","Fazer compras","Ler um livro"];
 
 // Request Body => /tarefas
@@ -21,18 +21,28 @@ router.get("/tarefas/:index", (req: Request, res: Response) => {
 router.post("/tarefas", (req: Request, res: Response) => {
   const { name } = req.body
 
-  
   if (!name) {
     res.status(400).json({ message: 'O nome da tarefa não pode ser vazio.' })
 
     return;
   }
 
-  
   tarefas.push(name)
 
   res.status(201).json({ message: "Tarefa adicionada com sucesso!", tarefa: name })
 })
 
+// Atualiza uma tarefa existente
+router.put("/tarefas/:index", (req: Request, res: Response) => {
+  const index = req.params.index
+  const { name } = req.body
 
-export { router }
+  if (!name) {
+    res.status(400).json({ message: 'O nome da tarefa não pode ser vazio.' })
+
+    return;
+  }
+
+  tarefas[Number(index)] = name
+  res.json({ message: "Tarefa atualizada com sucesso!", tarefa: name })
+})
